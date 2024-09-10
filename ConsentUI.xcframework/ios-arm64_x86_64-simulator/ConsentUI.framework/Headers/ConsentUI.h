99440fbc04ff71a314aae14ea63cd266fd099109
Rohan Chavda <rohan.chavda@securiti.ai>
2024-09-10 19:12:42 +0530

    Added xcframeworks and Swift Package Manager Configuration

---
+++ ConsentUI.xcframework/ios-arm64_x86_64-simulator/ConsentUI.framework/Headers/ConsentUI.h
@@ -0,0 +1,18 @@
+//
+//  ConsentUI.h
+//  ConsentUI
+//
+//  Created by Rohan Chavda on 25/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConsentUI.
+FOUNDATION_EXPORT double ConsentUIVersionNumber;
+
+//! Project version string for ConsentUI.
+FOUNDATION_EXPORT const unsigned char ConsentUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConsentUI/PublicHeader.h>
+
+
